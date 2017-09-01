62c6a26dc9b1f85f2949560a8f5216787d23dd4e
Piethein Strengholt <piethein@strengholt-online.nl>
2017-09-01 14:10:43 +0200

    asdd vue-resource and cleanup App.Vue

---
+++ src/components/Home.Vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container">
+        <h1>The home component</h1>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueResource from 'vue-resource';
+import App from './App.vue';
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'http://localhost/rssmonster/public/index.php/api/';
 
 new Vue({
   el: '#app',
