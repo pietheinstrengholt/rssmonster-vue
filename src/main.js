2a7a66c3d3d7dcfecb07566a62b6f742d233540f
Piethein Strengholt <piethein@strengholt-online.nl>
2017-12-26 16:01:49 +0100

    add vue-stash to implement central state management and watchers over central state

--- src/main.js
+++ src/main.js
@@ -6,7 +6,18 @@ Vue.use(VueResource);
 
 Vue.http.options.root = 'http://localhost/rssmonster/public/index.php/api/';
 
+import VueStash from 'vue-stash'
+Vue.use(VueStash)
+
 new Vue({
 	el: '#app',
+	data: {
+		store: {
+			data: {
+				category: 0,
+				feed: 0
+			}
+		}
+	},
 	render: h => h(App)
 });
